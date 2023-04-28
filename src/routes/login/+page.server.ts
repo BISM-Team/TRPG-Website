882763d536942a7b1be8def89c8d301b267a8f11
difanta <jackiedifant@gmail.com>
2023-04-28 12:52:49 +0200

    login/register form autocomplete

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -11,7 +11,7 @@ export const load = (async ({ locals }) => {
 export const actions: Actions = {
   login: async (event) => {
     const data = await event.request.formData();
-    const email = data.get("email")?.toString(); // email or name
+    const email = data.get("username")?.toString(); // email or name
     const password = data.get("password")?.toString();
     const redirect_url = data.get("redirect")?.toString();
 
@@ -40,7 +40,7 @@ export const actions: Actions = {
   register: async (event) => {
     const data = await event.request.formData();
     const email = data.get("email")?.toString();
-    const name = data.get("name")?.toString();
+    const name = data.get("username")?.toString();
     const password = data.get("password")?.toString();
     const repeat_password = data.get("repeat_password")?.toString();
     const redirect_url = data.get("redirect")?.toString();

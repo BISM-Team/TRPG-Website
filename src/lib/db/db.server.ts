975a6dfe65cc68408a28cdbe42b7c45668d3488d
difanta <jackiedifant@gmail.com>
2023-04-28 12:11:39 +0200

    add logging

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -9,4 +9,29 @@ const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
 
 class PrismaClient extends _PrismaClient {}
 
-export const db = new PrismaClient();
+export const db = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+db.$on("query", (e) => {
+  console.log("Query: " + e.query);
+  console.log("Params: " + e.params);
+  console.log("Duration: " + e.duration + "ms");
+});

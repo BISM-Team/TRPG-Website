72ab6b68f0588bf862482f62140b5ca93c46d6a4
difanta <jackiedifant@gmail.com>
2023-05-18 15:14:38 +0200

    env based logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,4 @@
+import { LOG_LEVEL } from "$env/static/private";
 import { deleteToken, getUserFromToken } from "$lib/db/auth.server";
 import type { Handle, HandleServerError } from "@sveltejs/kit";
 
@@ -5,14 +6,16 @@ export const handle = async function ({ event, resolve }) {
   const token = event.cookies.get("Authorization");
   event.locals.user = await getUserFromToken(token);
   if (!event.locals.user && token) deleteToken(event.cookies);
-  const before = Date.now();
-  const result = await resolve(event);
-  const after = Date.now();
-  const url = new URL(event.request.url);
-  console.log(
-    `Request ${event.request.method} ${url.pathname} took ${after - before}ms`
-  );
-  return result;
+  if (LOG_LEVEL.includes("REQUEST")) {
+    const before = Date.now();
+    const result = await resolve(event);
+    const after = Date.now();
+    const url = new URL(event.request.url);
+    console.log(
+      `Request ${event.request.method} ${url.pathname} took ${after - before}ms`
+    );
+    return result;
+  } else return await resolve(event);
 } satisfies Handle;
 
 export const handleError = async function ({ error }) {

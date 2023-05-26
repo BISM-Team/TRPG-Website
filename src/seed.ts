4b200ef267985112f9572e6f549609a9e862e17e
Tommaso Di Fant <jackiedifant@gmail.com>
2023-05-26 10:46:28 +0200

    ,

--- src/seed.ts
+++ src/seed.ts
@@ -159,3 +159,13 @@ async function main() {
     })
   );
 }
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });

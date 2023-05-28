d8c651d063174105e710f24d559d3ca6a5d118cd
Tommaso Di Fant <jackiedifant@gmail.com>
2023-05-28 21:14:33 +0200

    test logout

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,8 +1,10 @@
 import { deleteToken } from "$lib/db/auth.server";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ locals, cookies }) => {
   if (locals.user) {
     deleteToken(cookies);
+    throw redirect(308, "/");
   }
 }) satisfies PageServerLoad;

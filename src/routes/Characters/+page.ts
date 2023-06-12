24723e15d35cf2eee8a5d95a79e8382e6159498f
difanta <jackiedifant@gmail.com>
2023-06-12 14:18:35 +0200

    Delete src/routes/Characters directory

--- src/routes/Characters/+page.ts
+++
@@ -1,9 +0,0 @@
-import { propagateErrors } from "$lib/utils";
-import type { PageLoad } from "./$types";
-
-export const load = (async ({ fetch, url }) => {
-  const response = await fetch("/api/characters");
-  await propagateErrors(response, url);
-  if (!response.ok) throw new Error("unexpected error");
-  return { characters: (await response.json()).characters };
-}) satisfies PageLoad;

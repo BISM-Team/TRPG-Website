1ddafe25d0f200afcb841f7e6256de9cd163dc10
difanta <jackiedifant@gmail.com>
2024-04-04 19:52:49 +0200

    add speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
   import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
   import '@skeletonlabs/skeleton/styles/skeleton.css';
-  import "../app.postcss";
-  import "$lib/components/Wiki/Heading.svelte";
+  import '../app.postcss';
+  import '$lib/components/Wiki/Heading.svelte';
   import { LightSwitch, storePopup } from '@skeletonlabs/skeleton';
   import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
-  import { goto } from "$app/navigation";
-  import type { LayoutData } from "./$types";
-  import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+  injectSpeedInsights();
+
+  import { goto } from '$app/navigation';
+  import type { LayoutData } from './$types';
+  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
   import { page } from '$app/stores';
-  import type { fetch as kit_fetch } from "@sveltejs/kit";
+  import type { fetch as kit_fetch } from '@sveltejs/kit';
   export let data: LayoutData;
 
   async function logout() {
-    const response = await (fetch as typeof kit_fetch)("/api/logout", { method: "POST" });
-    if (response.ok) await goto("/", { invalidateAll: true });
+    const response = await (fetch as typeof kit_fetch)('/api/logout', { method: 'POST' });
+    if (response.ok) await goto('/', { invalidateAll: true });
   }
 
   function isHome() {
-    return $page.url.pathname === "/";
+    return $page.url.pathname === '/';
   }
 
   function isActiveRoute(path: string) {
@@ -29,22 +32,36 @@
 </script>
 
 <AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
-	<svelte:fragment slot="header">
-    <AppBar >
+  <svelte:fragment slot="header">
+    <AppBar>
       <svelte:fragment slot="lead">
         <h1 class="h2">BISM Website</h1>
       </svelte:fragment>
       <nav class="list-nav">
-        <ul class="flex flex-row w-full justify-evenly">
+        <ul class="flex w-full flex-row justify-evenly">
           {#key $page.url}
             <li><a class="btn" href="/" class:bg-primary-active-token={isHome()}>Home</a></li>
-            <li><a class="btn" href="/characters" class:bg-primary-active-token={isActiveRoute("/characters")}>Characters</a></li>
-            <li><a class="btn" href="/campaign" class:bg-primary-active-token={isActiveRoute("/campaign")}>Campaign</a></li>
+            <li>
+              <a
+                class="btn"
+                href="/characters"
+                class:bg-primary-active-token={isActiveRoute('/characters')}>Characters</a
+              >
+            </li>
+            <li>
+              <a
+                class="btn"
+                href="/campaign"
+                class:bg-primary-active-token={isActiveRoute('/campaign')}>Campaign</a
+              >
+            </li>
             <li>
               {#if data.auth}
                 <button class="btn" on:click={logout}>Logout</button>
               {:else}
-                <a class="btn" href="/login" class:bg-primary-active-token={isActiveRoute("/login")}>Login/Signup</a>
+                <a class="btn" href="/login" class:bg-primary-active-token={isActiveRoute('/login')}
+                  >Login/Signup</a
+                >
               {/if}
             </li>
           {/key}
@@ -55,7 +72,7 @@
       </svelte:fragment>
     </AppBar>
   </svelte:fragment>
-	<slot />
+  <slot />
 </AppShell>
 
 <style lang="postcss">

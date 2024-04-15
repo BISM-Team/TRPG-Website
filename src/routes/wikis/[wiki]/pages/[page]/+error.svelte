26232f7397eeba814003229c1caf3bde52e5858b
difanta <jackiedifant@gmail.com>
2024-04-15 18:41:20 +0200

    wiki error page fixed

--- src/routes/wikis/[wiki]/pages/[page]/+error.svelte
+++ src/routes/wikis/[wiki]/pages/[page]/+error.svelte
@@ -12,7 +12,7 @@
   afterNavigate(({ from, to }) => {
     previousUrl = from !== to ? from?.url.toString() : './index';
     if (from?.url.pathname.toString().endsWith(`/index`) && to?.url.toString().endsWith(`/index`)) {
-      goto(`/wikis/${$page.params.wiki}/pages/index`);
+      goto(`/`);
     }
     if ($page.status === 401) goto('/login');
   });
@@ -30,41 +30,25 @@
   };
 </script>
 
-<div id="content" class="p-2 text-center">
+<div id="content" class="text-center">
   {#if actualStatus === 200}
     <span />
   {:else if actualStatus === 404}
-    <h1 class="w3-section w3-padding h1">Page not yet created</h1>
-    <p>Do you want to create it?</p>
-    <form
-      id="btnContainer"
-      class="w3-container"
-      method="post"
-      action="?/create"
-      use:enhance={createPage}
-    >
+    <h2 class="m-xxl h2">Page not yet created</h2>
+    <p class="mt-l mb-m">Do you want to create it?</p>
+    <form id="btnContainer" method="post" action="?/create" use:enhance={createPage}>
       <button {disabled} class="btn-secondary" type="button" on:click={goBack}>No</button>
       <button {disabled} class="btn-primary" type="submit">Yes</button>
     </form>
   {:else if actualStatus === 403}
-    <h1 class="w3-section w3-padding h1">You are not allowed to view this page!</h1>
-    <button {disabled} class="btn-primary" type="submit">Yes</button>
+    <h2 class="m-xxl h2">You are not allowed to view this page!</h2>
+    <button {disabled} class="btn-secondary" type="button" on:click={goBack}>Go back</button>
   {:else if actualStatus === 500}
-    <h1 class="h1 text-center">Server Error, please try again.</h1>
+    <h2 class="m-xxl h2">Server Error, please try again.</h2>
   {:else if actualStatus === 400 && $page.error}
-    <h1 class="h1 text-center">{$page.error.message}</h1>
+    <h2 class="m-xxl h2">{$page.error.message}</h2>
   {:else}
-    <h1>Unknown Error, please try again.</h1>
+    <h2>Unknown Error, please try again.</h2>
     <p>{actualStatus}: {JSON.stringify($page.form)}</p>
   {/if}
 </div>
-
-<style lang="postcss">
-  h1 {
-    font-size: xx-large;
-  }
-
-  button {
-    width: 4em;
-  }
-</style>
